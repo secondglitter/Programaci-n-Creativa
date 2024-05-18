15cf91fdc5268df1787381bc3025facaca239db2
secondglitter <joshgonzbv@gmail.com>
2024-05-18 13:23:31 -0500

    se sube un nuevo codigo

--- python/practica 2-3.py
+++ python/practica 2-3.py
@@ -5,8 +5,36 @@ def setup():
     
     for x in range ( 100, 1000, 100):
         for y in range (100, 800, 100):
-            fill(255)
-            rect_mode(CENTER)
-            square(x,y, 50)
-            fill(0)
-            circle(x, y, 20)
+            n = random_int(1,6)
+            print(n, x, y)
+            if n <= 2:
+                fill(255)
+                rect_mode(CENTER)
+                square(x,y, 50)
+                fill(0)
+                rect_mode(CENTER)
+                square(x,y, 30)
+            elif n <= 4:
+                fill(255)
+                circle(x, y, 50)
+                fill(0)
+                circle(x, y, 30)
+            else:
+                p = random_int (5, 8)
+                fill(0)
+                star(x,y, 10, 30, p)
+                fill(255)
+                star(x,y, 5, 15, p)
+
+def star (cx, cy, ra, rb, np, start_ang=0):
+    step = TWO_PI / np
+    begin_shape()
+    for i in range(np):
+        ang = start_ang + step * i + frame_count / 50.0
+        ax = cx + cos(ang) * ra
+        ay = cy + sin(ang) * ra
+        vertex (ax, ay)
+        bx = cx  + cos(ang + step / 2.0) * rb
+        by = cy + sin(ang + step / 2.0) * rb
+        vertex (bx, by)
+    end_shape(CLOSE)
